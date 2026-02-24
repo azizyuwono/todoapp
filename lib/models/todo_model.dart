9362bc39e04faeb8c4746697861016d4533118cb
Aziz Yuwono <azizyuwono.2020@gmail.com>
2026-02-24 09:18:10 +0700

    add input

---
+++ lib/models/todo_model.dart
@@ -0,0 +1,8 @@
+class Todo{
+  String todo;
+  bool isDone;
+
+  Todo({required this.todo, required this.isDone});
+}
+
+
--- lib/pages/todo_page.dart
+++ lib/pages/todo_page.dart
@@ -1,23 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:todoapp/widgets/todo_item.dart';
 
-class TodoPage extends StatelessWidget {
+class TodoPage extends StatefulWidget {
   const TodoPage({super.key});
 
+  @override
+  State<TodoPage> createState() => _TodoPageState();
+}
+
+class _TodoPageState extends State<TodoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('My Todo')),
 
-      body: Column(
-        children: [
-          TodoItem()
-        ],
-      ),
+      body: Column(children: [TodoItem()]),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () => inputTodo(context),
       ),
     );
   }
+}
+
+void inputTodo(BuildContext context) {
+  final controller = TextEditingController();
+  showDialog(
+    context: context,
+    builder: (BuildContext context) => Dialog(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              controller: controller,
+              decoration: InputDecoration(hintText: 'Input Todo')),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: () {}, child: Text('Done')),
+          ],
+        ),
+      ),
+    ),
+  );
 }
